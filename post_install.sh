20249da6d469305125b1dee4af939b1da3915732
kuchosauronad0 <22005492+kuchosauronad0@users.noreply.github.com>
2019-09-17 20:42:08 -0700

    Add files

---
+++ post_install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Post install scrip for iocage-plugin-synapse
+# Copyright 2019 Andre Poley <andre.poley@mailbox.org> 
+echo <<EOT >>/etc/rc.conf
+synapse_enable=YES
+EOT
+mv /usr/local/etc/matrix-synapse/homeserver.yml /usr/local/etc/matrix-synapse/homeserver1.yml
+
+service synapse start
+
