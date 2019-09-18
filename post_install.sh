18762727e9f2fd190fea6d346513148bb1b16bba
kuchosauronad0 <22005492+kuchosauronad0@users.noreply.github.com>
2019-09-18 01:57:23 -0700

    Fixin

--- post_install.sh
+++ post_install.sh
@@ -2,12 +2,16 @@
 # Post install scrip for iocage-plugin-synapse
 # Copyright 2019 Andre Poley <andre.poley@mailbox.org> 
 
+
+echo "Generate homeserver.yml"
+rm /usr/local/etc/matrix-synapse/homeserver.yml
+/usr/local/bin/python3.6 -B -m synapse.app.homeserver -c /usr/local/etc/matrix-synapse/homeserver.yaml --generate-config --server-name=synapse.example.com --report-stats=no
+ln -s /usr/local/etc/matrix-synapse/homeserver.yaml /usr/local/etc/matrix-synapse/homeserver.yml
+
 echo "Add synapse to rc.conf"
 echo <<EOT >>/etc/rc.conf
-synapse_enable=YES
+synapse_enable="YES"
 EOT
-echo "Generate homeserver.yml"
-/usr/local/bin/python3.6 -B -m synapse.app.homeserver -c /usr/local/etc/matrix-synapse/homeserver.yml --generate-config --server-name=synapse.example.com --report-stats=no
 
 echo "Start synapse"
 service synapse start
