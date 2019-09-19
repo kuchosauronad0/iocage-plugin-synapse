a117fe18b1ca4231b69ee554a000f544ed199b96
kuchosauronad0 <22005492+kuchosauronad0@users.noreply.github.com>
2019-09-18 22:52:19 -0700

    Add variables

--- post_install.sh
+++ post_install.sh
@@ -1,27 +1,39 @@
 #!/bin/sh
-# Post install scrip for iocage-plugin-synapse
+# Post install script for iocage-plugin-synapse
 # Copyright 2019 Andre Poley <andre.poley@mailbox.org> 
+: ${matrix-synapse_name="matrix-synapse"}
+: ${matrix-synapse_user="synapse"}
+: ${matrix-synapse_data="/var/db/matrix-synapse"}
+: ${matrix-synapse_log="/var/log/matrix-synapse.log"}
+: ${matrix-synapse_listen="0.0.0.0,::"}
+: ${matrix-synapse_server="synapse.example.com"}
 
+# Generate a SSL key
+sudo mkdir -p /usr/local/etc/ssl/{keys,certs}
+chmod 0600 /usr/local/etc/ssl/keys
+openssl genrsa -out /usr/local/etc/ssl/keys/${matrix-synapse_server}.key 4096
 
-echo "Generate homeserver.yml"
+# Create the default directories
+mkdir -p ${matrix-synapse_data}
+mkdir -p ${matrix-synapse_data}/media_store
+mkdir -p ${matrix-synapse_data}/uploads
+chown ${matrix-synapse_user}:${matrix-synapse_user} ${matrix-synapse_data}
+
+# Generate homeserver.yml
 rm /usr/local/etc/matrix-synapse/homeserver.yml
-/usr/local/bin/python3.6 -B -m synapse.app.homeserver -c /usr/local/etc/matrix-synapse/homeserver.yaml --generate-config --server-name=synapse.example.com --report-stats=no
+/usr/local/bin/python3.6 -B -m synapse.app.homeserver -c /usr/local/etc/matrix-synapse/homeserver.yaml --generate-config --server-name=${matrix-synapse_server} --report-stats=no
+# Note: an existing homeserver.yml(but empty) is provided by the overlay; use this and comment line 23-24 if you like
 ln -s /usr/local/etc/matrix-synapse/homeserver.yaml /usr/local/etc/matrix-synapse/homeserver.yml
 
-echo "Add synapse to rc.conf"
-echo 'synapse_enable="YES"' >> /etc/rc.conf
-
-echo "Configure /var/db/matrix-synapse for uploads and storage"   
-mkdir -p /var/db/matrix-synapse/media_store
-mkdir -p /var/db/matrix-synapse/uploads
-chown -R synapse /var/db/matrix-synapse
-
 sed -i '' -e 's+^pid_file:.*$+pid_file: /var/run/matrix-synapse/synapse.example.com.pid+g' /usr/local/etc/matrix-synapse/homeserver.yaml
-sleep 5
 sed -i '' -e 's+^media_store_path:.*$+media_store_path: "/var/db/matrix-synapse/media_store"+g' /usr/local/etc/matrix-synapse/homeserver.yaml
-sleep 5
 sed -i '' -e 's+^uploads_path:.*$+uploads_path: "/var/db/matrix-synapse/uploads"+g' /usr/local/etc/matrix-synapse/homeserver.yaml
-sleep 5
 sed -i '' -e 's+^.*filename:.*+        filename: /var/log/matrix-synapse/synapse.example.com.log+g' /usr/local/etc/matrix-synapse/synapse.example.com.log.config
 
-echo "Done"
+sysrc -f /etc/rc.conf synapse_enable="YES"
+
+# Start the service
+if $(service synapse start 2>/dev/null >/dev/null) ; then
+    echo "Starting matrix-synapse."
+fi
+
